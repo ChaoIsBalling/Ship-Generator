65b9053521bb0254904c71139f851f2f98a7619a
Jiale <jialehe28@gmail.com>
2024-02-16 02:31:31 +0100

    d

--- button.js
+++ button.js
@@ -1,25 +1,22 @@
 function ChangeLiked()
 {
-var button = document.getElementsByClassName("Favorito");
-  for (var i = 0; i < button.length; i++) {
-    if (button.item(i).style.visibility == 'hidden') {
-      button.item(i).style.visibility = 'visible';
-    }
-    else button.item(i).style.visibility = 'hidden';
-  }
-}
-function ChangeDisliked()
-{
-    var button = document.getElementsByClassName("Menos (aunque igual muy favoritos)");
-    for (var i = 0; i < button.length; i++) {
-      if (button.item(i).style.visibility == 'hidden') {
-        button.item(i).style.visibility = 'visible';
-      }
-      else button.item(i).style.visibility = 'hidden';
-    }
-}
-function ChangeColors()
-{
-    var elemmento =document.getElementById("myspan");
-   elemmento.style="color:green";S
+  const months = ["Laios Touden", "Falin Touden", "Marcille Donato", "Chilchuck Tims", "Meijack Chils", 
+  "Fleurtom Chils", "Puckpatti Chils","Senshi of Izganda","Izutsumi","Toshiro 'Shuro' Nakamoto","Maizuru",
+ "Hien","Benchidori","Inutade","Namari of Kahka Brud","Tansu Floke","Yarn Floke","Kiki Floke","Kaka Floke","Kabru",
+"Mikbell Tomas","Kuro","Daya of Sadena","Daya's Fiancee","Holm Kranom", "Laios' Dad","Laios' Mom","Chilchuck's Wife",
+"Marcille's Mom","Marcille's Mom's Husband","Marcille's Dad","Mithrun's Brother","Toshiro's Dad","Toshiro's Mom",
+"The Winged Lion","Dandan","The Winged Lion","Chilchuck's Older Brother","Chilchuck's Older Sister"
+,"Chilchuck's younger brother","Chilchuck's youngest brother","Toshiro's younger brother","Toshiro's youngest brother",
+"Rinsha 'Rin' Fana","Mithrun of the House of Kerensil","Mithrun's Brother","Pattadol of the House of Vari",
+"Cithis Ofri","Fleki","Lycion","Otta","Milsiril of the House of Tol","Helki","Flamela of the House of Sorn","Fungil the Swift",
+"Coyote the Clairvoyant","Opa son of Mikepas","Keen-Eared Sita","Yugin of the Golden Voice","Pahsa","Pohsa","Snow",
+"Misyl","Erique","Gillin of Izganda","Brigan of Izganda","Totan of Dozahk","Invar of Izganda","Null Milchain","Thistle",
+"Freinag Merini","Delgal Merini","Eodio Merini","Yaad Merini","Zon","Leed","Bahay","Dotoy","Min","Gageh","Don","Ban",
+"Fionil","Doni"];
+
+  const random = Math.floor(Math.random() * months.length);
+
+  var Shipped=months[Math.floor(Math.random() * months.length)];
+  var Shipped2=months[Math.floor(Math.random() * months.length)];
+document.getElementById("p1").innerHTML = Shipped+ " X " + Shipped2;
 }
